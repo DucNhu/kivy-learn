f3e9e75d6a35c7c9c2211e9e4172ca1c3d8b7721
DESKTOP-KGSI2GA\DucNhu <ducnhu2k1@gmail.com>
2023-11-02 00:04:22 +0700

    test pyjnius

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+from jnius import autoclass
 from kivy.app import App
 from kivy.uix.label import Label
 
 class MyApp(App):
     def build(self):
-        label = Label(text="Hello world")
+        # Tạo label
+        label = Label(text="Trạng thái thiết bị")
+
+        # Lấy trạng thái thiết bị
+        screen = jnius.autoclass("android.view.Window").getDecorView().getSystemUiVisibility()
+
+        # Kiểm tra xem màn hình đang khóa hay không
+        if screen & jnius.autoclass("android.view.View").SYSTEM_UI_FLAG_LOW_PROFILE:
+            label.text = "Màn hình đang khóa"
+        else:
+            label.text = "Màn hình đang mở"
+
+        # Style label
+        label.pos = (100, 100)
+        label.halign = "left"
+        label.valign = "top"
         return label
 
 app = MyApp()
